8b3de022066b9ef2c2ba6ba623853e7cfc83322c
ellensakoda <ellen.sakoda@menloschool.org>
2026-03-03 13:37:05 -0800

    Added Last Name section to the program.

--- src/Backend.java
+++ src/Backend.java
@@ -1,8 +1,20 @@
+import com.sun.source.tree.ClassTree;
+
+import java.util.Scanner;
+
 public class Backend {
     private Frontend window;
+    private String lastName;
 
     public Backend() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Last Name: ");
+        this.lastName = input.nextLine();
         this.window = new Frontend(this);
+    }
+
+    public String getLastName(){
+        return lastName;
     }
 
     public static void main(String[] args) {
