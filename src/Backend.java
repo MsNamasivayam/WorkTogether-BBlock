f6905fda8e4e04fe0045339904c7a6a9a7f2ccd8
oliviaoccheng <oliviaoc.cheng@gmail.com>
2026-03-03 13:36:14 -0800

    Created a firstName instance variable and a drawFirstName function in the oc_firstName branch.

--- src/Backend.java
+++ src/Backend.java
@@ -1,12 +1,28 @@
+import java.util.Scanner;
+
 public class Backend {
     private Frontend window;
+    private Scanner input;
+    private String name;
 
     public Backend() {
         this.window = new Frontend(this);
+        input = new Scanner(System.in);
+        this.name = "";
+    }
+
+    public void setFirstName() {
+        System.out.println("What is your name?");
+        this.name = input.nextLine();
+        window.repaint();
+    }
+
+    public String getFirstName() {
+        return name;
     }
 
     public static void main(String[] args) {
         Backend backend = new Backend();
+        backend.setFirstName();
     }
-
 }
