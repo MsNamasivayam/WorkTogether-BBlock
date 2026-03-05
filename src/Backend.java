05382904fad1227c8f31e37558b10346917ed5a0
Alexis Mendoza <alexis.mendoza@menloschool.org>
2026-03-05 08:57:59 -0800

    Finished

--- src/Backend.java
+++ src/Backend.java
@@ -1,12 +1,25 @@
+import java.util.Scanner;
+
 public class Backend {
     private Frontend window;
+    private String meal = "";
 
     public Backend() {
         this.window = new Frontend(this);
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("What is your favorite meal?");
+        meal = s.nextLine();
+
+        window.repaint();
     }
 
     public static void main(String[] args) {
         Backend backend = new Backend();
+    }
+
+    public String getMeal(){
+        return meal;
     }
 
 }
